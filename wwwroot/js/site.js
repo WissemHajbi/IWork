8f73f4bb569878e27a67cde72069ed7d051fee4b
WissemHajbi <wissemhajbi2002@gmail.com>
2023-02-24 16:13:47 +0100

    Add: Activating the filter button chosen

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,22 @@
 ﻿var elements = document.querySelectorAll(".card");
 
 function changeContent(FieldName) {
+    // filtering the Job Offers by id
     elements.forEach(function (item) {
-        if (item.id != FieldName) {
-            item.style.display = "none";
-            console.log(" 1 i " + item.id + " fieldname = " + FieldName);
-        } else {
+        if (item.id == FieldName) {
             item.style.display = "block";
-            console.log(" 2 i " + item.id + " fieldname = " + FieldName);
+        } else {
+            item.style.display = "none";
+        }
+    });
+
+    // activating the filter button chosen
+    var button = document.querySelectorAll(".filterbtn");
+    button.forEach(function (btn) {
+        if (btn.name == `${FieldName}Button`) {
+            btn.classList.add("active");
+        } else {
+            btn.classList.remove("active");
         }
     });
 }
