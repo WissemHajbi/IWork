371fd9dab38fc6a5ebc36680c12f507451f18bdf
WissemHajbi <wissemhajbi2002@gmail.com>
2023-03-08 00:20:47 +0100

    Fix: backgeound and OffersID

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -61,3 +61,29 @@ offers_card_desc.forEach((el) => {
 //     desc.appendChild(lineBreak);
 //     desc.innerHTML += e;
 // });
+
+var textWrapper = document.querySelector(".ml13");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime
+    .timeline({ loop: true })
+    .add({
+        targets: ".ml13 .letter",
+        translateY: [100, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: (el, i) => 300 + 30 * i,
+    })
+    .add({
+        targets: ".ml13 .letter",
+        translateY: [0, -100],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 6000,
+        delay: (el, i) => 100 + 30 * i,
+    });
